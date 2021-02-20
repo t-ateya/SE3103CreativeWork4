7651247b34b503e15744dd1552a41ed3101a6d76
Terence Ateya <ateyaterence@gmail.com>
2021-02-20 07:59:48 -0600

    commit

--- model/interfaces/IProductMap.java
+++ model/interfaces/IProductMap.java
@@ -1,6 +1,6 @@
 package model.interfaces;
 
-public interface IOrderMap <String , T extends IOrder> {
+public interface IProductMap <String , T extends IOrder> {
 	public abstract void addOrder(String id, T t);
 	public abstract void cancelOrder(String id);
 	
